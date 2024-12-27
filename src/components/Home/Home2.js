0508fac9e8371aa6828b24a7aa447aed2d2bab97
vipins <https://gitlab.intoday.in>
2024-12-27 22:31:33 +0530

    update resume

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avt3.png";
+import myImg from "../../Assets/vp.png";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
